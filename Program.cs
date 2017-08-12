9f02736ead59dd377c59786842619770c22374f7
Uğur Deniz <ugur2353@gmail.com>
2017-08-12 17:56:56 +0300

    Try - Catch Örneği
    Try - Catch Örneği oluşturuldu

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRY_CATCH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Sayı Giriniz");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(Exception ex) when(ex.Source=="mscorlib")
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+            
+        }
+    }
+}
